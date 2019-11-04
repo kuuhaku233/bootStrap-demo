e41cd829b62ce1e8f122923d03deb7b649b51898
kuuhaku233 <bingxian666@gmail.com>
2019-11-04 11:39:42 +0800

    完成SQL查询 添加SysResource 没有完成ajax的调用

---
+++ src/com/bst/controller/SysResourceController.java
@@ -0,0 +1,24 @@
+package com.bst.controller;
+
+import com.bst.pojo.Resource;
+import com.bst.service.SysResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/system/")
+public class SysResourceController {
+    @Autowired
+    private SysResourceService sysResourceService;
+
+    @RequestMapping("getresource")
+    @ResponseBody
+    List<Resource> getresource()
+    {
+         return sysResourceService.getresource();
+    }
+}
